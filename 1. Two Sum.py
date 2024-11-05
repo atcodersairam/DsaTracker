b350f66abdf0bb430e2d427355061fe91cecf9a5
BN Sairam <139829666+atcodersairam@users.noreply.github.com>
2024-11-05 20:09:46 +0530

    Create 1. Two Sum.py

---
+++ 1. Two Sum.py
@@ -0,0 +1,19 @@
+#Brute-force
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        return []
+#Efficient-HashMap
+hash_map={}
+for i,num in enumerate(nums):
+  complement=target-num
+  if complement in hash_map:
+    return[hash_map[complement],i]
+  hashmap[nums]=i
+return[]
+
+        
